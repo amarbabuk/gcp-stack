dac0208186e0835e923244a9b3c8ce3bd1113cc5
amarbabu <amarbabu@gmail.com>
2022-10-04 17:09:20 +0530

    queries

--- drl/notes.py
+++ drl/notes.py
@@ -67,3 +67,45 @@ gs://spls/gsp394/tables/time_trial_group_pilot_crossings.csv
 gs://spls/gsp394/tables/time_trial_group_pilot_times.csv
 gs://spls/gsp394/tables/time_trial_group_pilots.csv
 gs://spls/gsp394/tables/time_trial_groups.csv
+
+
+Q1
+Events in a Certain City
+SELECT name FROM `causal-guide-108309.foo_drl.events` where city = 'Miami'
+
+Q2
+Event Pilot Names
+select name, first_name, last_name, e.pilot_id, event_id from `causal-guide-108309.foo_drl.pilots` p, `causal-guide-108309.foo_drl.event_pilots` e where p.id = e.pilot_id
+
+Q3
+Pilots Who Flew in an Event
+select p.name, v.name  from `causal-guide-108309.foo_drl.pilots` p, `causal-guide-108309.foo_drl.event_pilots` e, `causal-guide-108309.foo_drl.events` v 
+where p.id = e.pilot_id
+and v.id = e.event_id
+and parse_date('%Y-%m-%d', v.end_date) < current_date()
+
+Q4
+Average Time of Rank 1 Round Finish
+SELECT time
+ (timestamp_seconds
+   (CAST
+     (AVG
+       (UNIX_SECONDS
+         (PARSE_TIMESTAMP('%H:%M.%S', minimum_time))
+       )
+   AS INT64)
+   )
+ )
+ from `causal-guide-108309.foo_drl.round_standings`
+ where rank = '1'
+
+ Q5
+ Clean and Combine Time Trial Data
+ 
+ insert into `causal-guide-108309.foo_drl.time_trial_cleaned`(time_trial_group_pilot_times_id,time_trial_group_pilot_id,time_trial_group_id,round_id,time) values (
+select t.id time_trial_group_pilot_times_id, p.id time_trial_group_pilot_id, g.id time_trial_group_id, g.round_id round_id, 'time' time
+from `causal-guide-108309.foo_drl.time_trial_group_pilot_times` t, `causal-guide-108309.foo_drl.time_trial_group_pilots` p, `causal-guide-108309.foo_drl.time_trial_groups` g
+where t.time_trial_group_pilot_id = p.id
+and g.id = p.time_trial_group_id
+)
+
---
+++ drl/time_trial_cleaned.py
@@ -0,0 +1,28 @@
+from google.cloud import bigquery
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+# TODO(developer): Set table_id to the ID of the table to create.
+# table_id = "your-project.your_dataset.your_table_name"
+table_id = "causal-guide-108309.foo_drl.time_trial_cleaned"
+
+# time_trial_group_pilot_times_id,time_trial_group_pilot_id,time_trial_group_id,round_id,time
+
+job_config = bigquery.LoadJobConfig(
+    source_format=bigquery.SourceFormat.CSV, skip_leading_rows=1, autodetect=True,
+)
+
+file_path="./time_trial_cleaned.csv"
+
+with open(file_path, "rb") as source_file:
+    job = client.load_table_from_file(source_file, table_id, job_config=job_config)
+
+job.result()  # Waits for the job to complete.
+
+table = client.get_table(table_id)  # Make an API request.
+print(
+    "Loaded {} rows and {} columns to {}".format(
+        table.num_rows, len(table.schema), table_id
+    )
+)
